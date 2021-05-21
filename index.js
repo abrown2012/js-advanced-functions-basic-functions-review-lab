0ee863a95f799386af667dd2be3735486a0733c6
Agnieszka Brown <abrown2012@icloud.com>
2021-05-20 21:25:50 -0400

    commit tree bc8e313cbefe3c221495adfb18899c5028393d9e

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+const saturdayFun = (activity="roller-skate") => {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = (arg="go to the office") => {
+    return `This Monday, I will ${arg}.`
+}
+
+const wrapAdjective = (arg2='*') => {
+    return function(arg="special"){
+        return `You are ${arg2}${arg}${arg2}!`
+    }
+}
+
+let Calculator = {
+    add: function(a, b) {
+        return a + b;
+    },
+    subtract: function(a, b){
+        return a - b; 
+    },
+    multiply: function(a, b){
+        return a*b;
+    },
+    divide: function(a, b){
+        return a/b;
+    }}
+
+    function actionApplyer(a, b) {
+        for (let i = 0; i < b.length; i++) {
+          a = b[i](a);
+        }
+        return a;
+      }
+      
+
